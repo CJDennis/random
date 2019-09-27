5ade4f679bc324dc8c6cf4b1e72b5528c3cb126c
Daniel Klein <danielklein@airpost.net>
2019-09-27 23:10:41 +1000

    Pass the test

--- src/CjDennis/Random/Random.php
+++ src/CjDennis/Random/Random.php
@@ -3,11 +3,19 @@ namespace CjDennis\Random;
 
 class Random {
   public static function random_int() {
-    $bytes = openssl_random_pseudo_bytes(2);
-    return hexdec(bin2hex($bytes));
+    trigger_error(__METHOD__ . '() is deprecated. Use ' .__CLASS__. '::random_short_int() instead.', E_USER_DEPRECATED);
+    return static::random_short_int();
+  }
+
+  public static function random_short_int() {
+    return hexdec(static::random_hex_bytes(2));
   }
 
   public static function random_hex_bytes(int $count): string {
-    return bin2hex(openssl_random_pseudo_bytes($count));
+    return bin2hex(static::random_bytes($count));
+  }
+
+  protected static function random_bytes(int $count) {
+    return openssl_random_pseudo_bytes($count);
   }
 }
---
+++ tests/unit/CjDennis/Random/RandomSeam.php
@@ -0,0 +1,38 @@
+<?php
+namespace CjDennis\Random;
+
+class RandomSeam extends Random {
+  protected static $bytes = '';
+  protected static $repeat_bytes = false;
+
+  public static function set_bytes(string $bytes) {
+    self::$bytes .= $bytes;
+  }
+
+  public static function repeat_bytes() {
+    self::$repeat_bytes = true;
+  }
+  
+  public static function do_not_repeat_bytes() {
+    self::$repeat_bytes = false;
+  }
+
+  protected static function random_bytes(int $count) {
+    if (static::$bytes === '') {
+      $random_bytes = parent::random_bytes($count);
+    }
+    else {
+      $min = min(strlen(static::$bytes), $count);
+      $random_bytes = substr(static::$bytes, 0, $min);
+      static::$bytes = substr(static::$bytes, $min);
+      if (static::$repeat_bytes) {
+        static::$bytes .= $random_bytes;
+      }
+      $count -= $min;
+      if ($count) {
+        $random_bytes .= static::random_bytes($count);
+      }
+    }
+    return $random_bytes;
+  }
+}
